d03bd4d2874709d9dce2725b5be66ba740a8c483
‘Tim <timothy.j.ocampo@gmail.com>
2018-07-24 19:02:23 -0700

    SASS WORKS! : 1.) needed to install all the loaders(style/css/sass), 2.) Add the rules to webpack.config.js, 3.)installed npm i --save node-sass-chokidar, 4.) adjust to scripts in package.json to compile. 5.) read the examples section @ https://www.npmjs.com/package/node-sass-chokidar for the sytax required to declare where you want to look for the main.scss file & where you want to send the main.css: right now, I'm using the main.css to import into the app and the App.scss is used as master control for styling. ALSO , if you look inside App.scss, there's an example of importing a partial

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,8 +22,18 @@ module.exports={
             {
                 test: /\.css$/,
                 use:['style-loader', 'css-loader']
-            }
+            },
 
+            {
+                test: /\.scss$/,
+                use:[
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "sass-loader",
+                        options:{}
+                    }
+                ]
+            }
 
         ]
     },
